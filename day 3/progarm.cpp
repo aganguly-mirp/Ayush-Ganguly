2468b2788b9a61e41f24c13de63d3459ec17f7e3
aganguly-mirp <aganguly-mirp@mit.edu>
2017-05-31 06:35:15 +0530

    Day 3

---
+++ day 3/progarm.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int p,n;
+cout<<"Enter the number"<<endl;
+cin>>n;
+int i=1;
+while (i<=10){
+p=i*n;
+cout<<p<<endl;
+i++;
+}
+return 0;
+}
+
